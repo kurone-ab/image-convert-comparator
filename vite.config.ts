04a20428789b4f2f202e0f1d97128d9a98a044ad
クロネ <ab@kurone.dev>
2024-12-24 02:28:15 +0900

    fix: jxl, heif dynamic library를 사용할 수 없는 문제 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, Plugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const viteServerConfig: Plugin = {
   name: 'log-request-middleware',
@@ -13,12 +13,29 @@ const viteServerConfig: Plugin = {
       res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
       next();
     });
-  }
+  },
 };
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss(), viteServerConfig],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        jxl: 'wasm-vips/vips-jxl.wasm?init',
+        heif: 'wasm-vips/vips-heif.wasm?init',
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.names[0].endsWith('.wasm')) {
+            return 'assets/[name][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['wasm-vips'],
   },
